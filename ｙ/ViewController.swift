593c50c24511a0b0d33eeaaa1959f414dd75dfce
rubysyosinsyadayo <my_child7@yahoo.co.jp>
2020-07-04 19:05:05 +0900

    countup_app

--- ｙ/ViewController.swift
+++ ｙ/ViewController.swift
@@ -9,12 +9,59 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var countlabel: UILabel!
+    
+    var count = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //らべる0に表示
+        countlabel.text="0"
     }
 
+    
+    
+    
+    @IBAction func plus(_ sender: Any) {
+   
+        count = count + 1
+       
+        countlabel.text=String(count)
+        if count >= 10  {
+               changeColor()
+               }
+        
+    }
+    
 
+    
+
+    @IBAction func minus(_ sender: Any) {
+    
+        count = count - 1
+               
+               countlabel.text=String(count)
+        
+           
+        if count <= 0  {
+        resetColor()
+        }
+        
+    }
+    
+    
+    func changeColor(){
+        countlabel.textColor = .red
+    }
+    func resetColor(){
+           countlabel.textColor = .white
+       }
+
+    
 }
+
+
 
