1bc497baff250766049358b3359dbef7355e85b5
rubysyosinsyadayo <my_child7@yahoo.co.jp>
2020-07-04 16:27:02 +0900

    Initial Commit

---
+++ ｙ/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ｙ
+//
+//  Created by a on 2020/07/04.
+//  Copyright © 2020 a. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
